e2799cf354d03966561717183dc70e37d2df3779
LaShawn <lashawnsomerville@LaShawns-MacBook-Pro.local>
2020-11-24 09:54:19 +0900

    Added return statement to DoctorController

--- src/controller/DoctorController.ts
+++ src/controller/DoctorController.ts
@@ -7,20 +7,19 @@ export class DoctorController {
     private doctorRepository = getRepository(Doctor);
 
     async all(request: Request, response: Response, next: NextFunction) {
-        return this.doctorRepository.find();
+        return await this.doctorRepository.find();
     }
 
     async one(request: Request, response: Response, next: NextFunction) {
-        return this.doctorRepository.findOne(request.params.id);
+        return await this.doctorRepository.findOne(request.params.id);
     }
 
     async save(request: Request, response: Response, next: NextFunction) {
-        return this.doctorRepository.save(request.body);
+        return await this.doctorRepository.save(request.body);
     }
 
     async remove(request: Request, response: Response, next: NextFunction) {
-        let doctorToRemove = await this.doctorRepository.findOne(request.params.id);
-        await this.doctorRepository.remove(doctorToRemove);
+        return await this.doctorRepository.delete({id:request.params.id});
     }
 
 }
